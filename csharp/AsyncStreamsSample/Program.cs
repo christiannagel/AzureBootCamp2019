5e9d2cca844f050f0cf747bbf2d69e02b3cb5456
Christian Nagel <christian@christiannagel.com>
2019-04-26 10:25:10 +0200

    async streams

---
+++ csharp/AsyncStreamsSample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncStreamsSample
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            //var aDevice = new ADevice();
+            //await foreach (var x in aDevice.GetSensorData1())
+            //{
+            //    Console.WriteLine($"{x.Value1} {x.Value2}");
+            //}
+
+            var cts = new CancellationTokenSource();
+            cts.CancelAfter(5000);
+            cts.Token.Register(() => Console.WriteLine("cancellation requested"));
+            var aDevice = new ADevice();
+            await foreach (var x in aDevice.GetSensorData(cts.Token))
+            {
+                Console.WriteLine($"{x.Value1} {x.Value2}");
+            }
+            Console.WriteLine("finished");
+        }
+    }
+}
