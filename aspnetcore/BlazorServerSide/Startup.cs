37943aaa6bbb9eb1ffa4bd570c8e407864251650
Christian Nagel <christian@christiannagel.com>
2019-04-27 10:15:35 +0200

    cleanup

--- aspnetcore/BlazorServerSide/Startup.cs
+++ aspnetcore/BlazorServerSide/Startup.cs
@@ -37,17 +37,6 @@ namespace BlazorServerSide
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseDeveloperExceptionPage();
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
-            //else
-            //{
-            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            //   // app.UseHsts();
-            //}
-
-            // app.UseHttpsRedirection();
 
             app.UseStaticFiles();
 
